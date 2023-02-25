fc8c715b8779f408b2bc6bad046c3c1bbae34062
Andre Lucas <alucas748@gmail.com>
2023-02-25 01:38:03 -0300

    Iniciando projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+# App - Preço do Bitcoin
+
++ Projeto flutter
+
+Desenvolvendo um aplicativo que mostre dinamicamente o preço do bitcoin com o auxílio de uma api.
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:preco_bitcoin/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
