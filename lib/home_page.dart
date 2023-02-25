13f556b4ebf5b1d59674b2275627baec8eb8d5ba
Andre Lucas <alucas748@gmail.com>
2023-02-25 01:57:08 -0300

    Interface do aplicativo criada

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,8 +8,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final String _preco = "0";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset("assets/images/bitcoin.png"),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, bottom: 30),
+                child: Text(
+                  "R\$ $_preco",
+                  style: const TextStyle(
+                    fontSize: 35
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                  padding: const EdgeInsets.fromLTRB(30, 15, 30, 15)
+                ),
+                child: const Text(
+                  "Atualizar",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
